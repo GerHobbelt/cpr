6083ce492e260fa45432e62c080bf110e743e534
Eugen Wintersberger <wintersberger@unisoftwareplus.com>
2020-06-08 16:45:42 +0200

    Fixing the package file

--- cpr-config.cmake
+++ cpr-config.cmake
@@ -10,17 +10,4 @@
 #                     to cpr.h, which must be included in every
 #                     file that uses this interface
 
-find_path(CPR_INCLUDE_DIR
-          NAMES cpr.h)
-
-find_library(CPR_LIBRARY
-             NAMES cpr
-             HINTS ${CPR_LIBRARY_ROOT})
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(CPR REQUIRED_VARS CPR_LIBRARY CPR_INCLUDE_DIR)
-
-if(CPR_FOUND)
-    set(CPR_LIBRARIES ${CPR_LIBRARY})
-    set(CPR_INCLUDE_DIRS ${CPR_INCLUDE_DIR})
-endif()
+include("${CMAKE_CURRENT_LIST_DIR}/cpr_targets.cmake")
